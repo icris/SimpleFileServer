05fc38f66779d2e3cf492d6643d88293f0a1a011
icris <icris@live.cn>
2017-04-30 01:02:23 +0800

    slight change in index.js

--- index.js
+++ index.js
@@ -10,19 +10,19 @@ const port = process.argv[3] || 8000;
 const publicDir = process.argv[2] || '/data';
 
 
-const favicon = serveFavicon('favicon.ico')
+const favicon = serveFavicon('favicon.ico');
 const logger = morgan('combined');
-const index = serveIndex(publicDir, { 'icons': true, 'view': 'details' })
-const serve = serveStatic(publicDir)
+const index = serveIndex(publicDir, { 'icons': true, 'view': 'details' });
+const serve = serveStatic(publicDir);
 
 
 http.createServer((req, res) => {
-    let done = finalhandler(req, res)
-    favicon(req, res, function onNext(err) {
-        if (err) return done(err)
-        logger(req, res, function(err) {
+    let done = finalhandler(req, res);
+    favicon(req, res, err => {
+        if (err) return done(err);
+        logger(req, res, err => {
             if (err) return done(err);
-            serve(req, res, function onNext(err) {
+            serve(req, res, err => {
                 if (err) return done(err);
                 index(req, res, done);
             });
